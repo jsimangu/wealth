60d3c18d5fb0fdb71c596b1c4100c310f47f6d46
jsimangu <78509719+jsimangu@users.noreply.github.com>
2025-10-07 20:23:13 -0700

    Update BijiTerminal.jsx

--- components/BijiTerminal.jsx
+++ components/BijiTerminal.jsx
@@ -1071,8 +1071,7 @@ const clients = dbClients.length > 0 ? dbClients : [
         </div>
       </div>
     </div>
-  );
-};
+  
 {/* Add Client Modal */}
 {showAddClientModal && (
   <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 p-4">
